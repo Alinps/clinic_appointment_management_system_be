f0b20fa2e57429b484c67a5efad3611074de7544
alin <alinps8406@gmail.com>
2026-06-18 17:43:59 +0530

    Add patient update functionality with API view and serializers

--- patients/urls.py
+++ patients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import (PatientCreateAPIView)
+from .views import (PatientCreateAPIView, PatientUpdateAPIView)
 
 urlpatterns = [
-    path("create/", PatientCreateAPIView.as_view(), name="patient-create")
+    path("create/", PatientCreateAPIView.as_view(), name="patient-create"),
+    path("<int:pk>/update/", PatientUpdateAPIView.as_view(), name="patient-update")
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,6 +7,7 @@ from rest_framework import status
 from accounts.permissions import  IsAdminOrReceptionist
 
 from .serializers import PatientSerializer
+from .models import Patient
 
 
 class PatientCreateAPIView(APIView):
@@ -26,4 +29,49 @@ class PatientCreateAPIView(APIView):
             },
             status=status.HTTP_201_CREATED
         )
+    
 
+
+class PatientUpdateAPIView(APIView):
+
+    permission_classes = [IsAdminOrReceptionist]
+
+    def put(self, request, pk):
+
+        patient = get_object_or_404(Patient,pk=pk)
+
+        serializer = PatientSerializer(patient, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(
+            {
+                "success":"True",
+                "message": "Patient updated successfully.",
+                "data":serializer.data
+            },
+            status=status.HTTP_200_OK
+        )
+    
+
+    def patch(self, request, pk):
+
+        patient = get_object_or_404(Patient, pk=pk)
+
+        serializer = PatientSerializer(patient, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(
+            {
+                "success":True,
+                "message":"Patient updated successfully.",
+                "data":serializer.data
+            },
+            status=status.HTTP_200_OK
+        )
+    
